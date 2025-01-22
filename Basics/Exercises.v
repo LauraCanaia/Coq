5b7580a6b85ebf3789ea100f3955e6281a61498a
Laura Canaia <45235539+LauraCanaia@users.noreply.github.com>
2025-01-22 15:47:04 +0100

    Update Exercises.v

--- Basics/Exercises.v
+++ Basics/Exercises.v
@@ -19,3 +19,17 @@ b : bool
 rewrite -> H: from f (f b) = b I'll go to f b = b
 
 rewrite -> H: from f b = b I'll go to b = b and with refexivity I'll demonstrate the proof*)
+
+Theorem negation_fn_applied_twice :
+  forall (f : bool -> bool),
+  (forall (x : bool), f x = negb x) -> forall (b : bool), f (f b) = b.
+ Proof.
+  intros f H b.
+  rewrite -> H.
+  rewrite -> H.
+  destruct b.
+  - reflexivity.
+  - reflexivity.
+Qed.
+
+(*Similar to the previous exercise but when I reach the section where I have to negate I check the true case and the false one.*)
